df41bab72cec1652ea7b118d4d9ff8f2c90061ce
Vithun V <vithun.v@dhyan.com>
2024-08-16 14:28:38 +0530

    deleting old

--- OldArchive/Spring/src/main/java/com/dhyan/dhyancc/security/RoleRepo.java
+++
@@ -1,9 +0,0 @@
-package com.dhyan.dhyancc.security;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepo extends JpaRepository<Role, Long> {
-
-}
